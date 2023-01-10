e9bca869adf9464417285131bfa298e9ea9ec746
s22024 <s22024@std.it-college.ac.jp>
2023-01-10 08:03:16 +0000

    chapter3

--- components/logo.js
+++ components/logo.js
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import styles from 'styles/logo.module.css'
 
-const Logo = ({ boxOn = false}) => 
+const Logo = ({ boxOn = false }) => 
     <Link href="/" legacyBehavior>
         <a className={boxOn ? styles.box : styles.basic}>CUBE</a>
     </Link>
