eeb9af9968b3ac58d6b99c314ca72c5d28964f28
s22024 <s22024@std.it-college.ac.jp>
2023-01-11 10:17:33 +0000

    サイドバー設定

---
+++ components/contact.js
@@ -0,0 +1,9 @@
+import styles from 'styles/contact.module.css'
+
+const Contact = () =>
+    <div className={styles.stack}>
+        <h3 className={styles.heading}>Contact</h3>
+        <address>cude@web.mail.address</address>
+    </div>
+
+export default Contact
