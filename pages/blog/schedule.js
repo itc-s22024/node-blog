95247eef05af6d2e4962aea637b6afde67e09b40
s22024 <s22024@std.it-college.ac.jp>
2023-02-02 07:58:57 +0000

    記事の本文追加

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,6 +1,9 @@
 import { getPostBySlug } from "lib/api"
 import Container from "@/components/container"
 import PostHeader from "@/components/post-header"
+import PostBody from "@/components/post-body"
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from "@/components/two-column"
+import Image from "next/image"
 
 export default function Schedule({
     title,
@@ -13,6 +16,28 @@ export default function Schedule({
         <Container>
             <article>
                 <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+                <figure>
+                    <Image
+                        src={eyecatch.url}
+                        alt=""
+                        width={eyecatch.width}
+                        height={eyecatch.height}
+                        style={{
+                            width: '100%',
+                            height: 'auto',
+                        }}
+                        sizes="(min-width: 1152px) 1152px, 100vw"
+                        priority
+                    />
+                </figure>
+                <TwoColumn>
+                    <TwoColumnMain>
+                        <PostBody>
+                            <div dangerouslySetInnerHTML={{ __html: content }} />
+                        </PostBody>
+                    </TwoColumnMain>
+                    <TwoColumnSidebar />
+                </TwoColumn>
             </article>
         </Container>
     )
